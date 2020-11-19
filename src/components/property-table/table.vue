6400c2dec07d9ffe4f6e9f5b8dcb5559294aabd3
zzj <zhjlydia@foxmail.com>
2020-11-19 23:55:36 +0800

    chore: 上传图片功能

--- src/components/property-table/table.vue
+++ src/components/property-table/table.vue
@@ -1,68 +1,10 @@
 <template>
-  <el-card
-    class="prop-table"
-    shadow="never"
-    body-style="font-size: 14px;text-align: left;"
-  >
+  <div class="prop-table">
     <div class="hidden-columns">
       <slot />
     </div>
-    <div
-      class="prop-table-header"
-      slot="header"
-    >
-      <slot name="header">
-        <span class="title">{{title}}</span>
-        <div class="prop-table-header-menu">
-          <template v-if="isEdit">
-            <el-tooltip content="提交">
-              <i
-                class="el-icon-check"
-                @click="onSubmit"
-              ></i>
-            </el-tooltip>
-            <el-tooltip content="放弃修改">
-              <i
-                class="el-icon-close"
-                @click="onDiscard"
-              ></i>
-            </el-tooltip>
-          </template>
-          <template v-else>
-            <el-tooltip
-              v-if="allowRefresh"
-              content="刷新"
-            >
-              <i
-                class="el-icon-refresh"
-                @click="onRefresh"
-              ></i>
-            </el-tooltip>
-            <el-tooltip
-              v-if="allowEdit"
-              content="编辑"
-            >
-              <i
-                class="el-icon-edit"
-                @click="setEdit(true)"
-              ></i>
-            </el-tooltip>
-            <el-tooltip
-              v-if="allowRemove"
-              content="删除"
-            >
-              <i
-                class="el-icon-delete"
-                @click="onRemove"
-              ></i>
-            </el-tooltip>
-          </template>
-          <slot
-            name="menu-item"
-            :isEdit="isEdit"
-          ></slot>
-        </div>
-      </slot>
+    <div class="prop-table-header" slot="header">
+      <span class="title">{{ title }}</span>
     </div>
     <div class="prop-table-body">
       <component
@@ -79,7 +21,51 @@
     <div class="prop-table-footer">
       <slot name="footer" />
     </div>
-  </el-card>
+    <div class="prop-table-menu">
+      <template v-if="isEdit">
+        <el-tooltip content="提交" placement="right">
+          <div class="menu-item">
+            <i class="el-icon-check" @click="onSubmit"></i>
+          </div>
+        </el-tooltip>
+        <el-tooltip content="放弃修改" placement="right">
+          <div class="menu-item">
+            <i class="el-icon-close" @click="onDiscard"></i>
+          </div>
+        </el-tooltip>
+        <el-tooltip content="上传文件" placement="right">
+          <div class="menu-item">
+            <el-upload
+              action="/api/share/upload"
+              :data="{ folderName: model }"
+              :show-file-list="false"
+              :on-success="uploadSuccess.bind(this)"
+            >
+              <i class="el-icon-picture-outline-round"></i>
+            </el-upload>
+          </div>
+        </el-tooltip>
+      </template>
+      <template v-else>
+        <el-tooltip v-if="allowRefresh" content="刷新" placement="right">
+          <div class="menu-item">
+            <i class="el-icon-refresh" @click="onRefresh"></i>
+          </div>
+        </el-tooltip>
+        <el-tooltip v-if="allowEdit" content="编辑" placement="right">
+          <div class="menu-item">
+            <i class="el-icon-edit" @click="setEdit(true)"></i>
+          </div>
+        </el-tooltip>
+        <el-tooltip v-if="allowRemove" content="删除" placement="right">
+          <div class="menu-item">
+            <i class="el-icon-delete" @click="onRemove"></i>
+          </div>
+        </el-tooltip>
+      </template>
+      <slot name="menu-item" :isEdit="isEdit"></slot>
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop, Emit, Watch } from 'vue-property-decorator'
@@ -96,6 +82,9 @@ let tableId = 1
   }
 })
 export default class extends Vue {
+  @Prop({ type: String })
+  model: string
+
   @Prop({ type: String })
   title: string
 
@@ -128,7 +117,6 @@ export default class extends Vue {
 
   @store.State()
   props: Property[]
-
 
   @store.State()
   isEdit: boolean
@@ -198,6 +186,16 @@ export default class extends Vue {
     this.setEdit(false)
   }
 
+  /**
+   * 文件上传成功后的回调函数
+   */
+  private uploadSuccess(res: any, file: File) {
+    this.$alert(res, '', {
+      confirmButtonText: '确定',
+      customClass: 'upload-success-modal'
+    })
+  }
+
   onSubmit() {
     this.$emit('submit', this.context)
     this.setEdit(false)
@@ -210,25 +208,40 @@ export default class extends Vue {
   position: absolute;
   z-index: -1;
 }
-.prop-table-header {
-  font-size: 20px;
-  display: flex;
-  justify-content: space-between;
-  .title {
-    font-weight: bold;
-  }
-  .prop-table-header-menu {
+.prop-table {
+  position: relative;
+  background: #fff;
+  border-radius: 10px;
+  padding: 10px 30px;
+  .prop-table-header {
+    height: 60px;
+    line-height: 60px;
+    margin-bottom: 30px;
+    border-bottom: 1px dashed #ccc;
+    font-size: 20px;
     display: flex;
-    justify-content: flex-end;
-    align-items: flex-end;
-    & > i,
-    &::v-deep .menu-button {
-      cursor: pointer;
-      margin: 0 12px;
-      &:hover {
-        color: #409eff;
-      }
+    justify-content: space-between;
+    .title {
+      font-weight: bold;
     }
+  }
+  .prop-table-menu {
+    position: absolute;
+    right: -40px;
+    top: 100px;
+    .menu-item {
+      width: 30px;
+      height: 30px;
+      line-height: 30px;
+      border-radius: 30px;
+      background: #fff;
+      margin-bottom: 5px;
+      text-align: center;
+    }
+  }
+
+  .upload-success-modal .el-message-box__message p {
+    word-break: break-all !important;
   }
 }
 </style>
--- src/views/models/article.vue
+++ src/views/models/article.vue
@@ -1,57 +1,42 @@
 <template>
   <el-main>
     <div class="model-wrap">
-    <property-table
-      v-if="item"
-      allow-refresh
-      allow-edit
-      :title="title"
-      :value="item"
-      :edit-mode="mode!=='view'"
-      @refresh="reload"
-      @submit="submit"
-      @json-error="onJsonError"
-      @remove="remove"
-    >
-      <property
-        prop="title"
-        name="文章标题"
-      />
-      <property
-        prop="description"
-        name="文章摘要"
-        type="textarea"
-      />
-      <property
-        prop="image"
-        name="主图"
-        type="image"
-        :options="{folder:'article'}"
-      />
-      <property
-        prop="categoryId"
-        name="类别"
-        type="enum"
-        :options="{ options: item.extra.category }"
-      />
-      <property
-        prop="tagIds"
-        name="标签"
-        type="check"
-        :options="{options: item.extra.tag}"
-      />
-      <property
-        prop="createdAt"
-        name="创建时间"
-        readonly
-        type="date"
-      />
-      <property
-        prop="content"
-        name="文章内容"
-        type="editor"
-      />
-    </property-table>
+      <property-table
+        :model="model"
+        v-if="item"
+        allow-refresh
+        allow-edit
+        :title="title"
+        :value="item"
+        :edit-mode="mode !== 'view'"
+        @refresh="reload"
+        @submit="submit"
+        @json-error="onJsonError"
+        @remove="remove"
+      >
+        <property prop="title" name="文章标题" />
+        <property prop="description" name="文章摘要" type="textarea" />
+        <property
+          prop="image"
+          name="主图"
+          type="image"
+          :options="{ folder: 'article' }"
+        />
+        <property
+          prop="categoryId"
+          name="类别"
+          type="enum"
+          :options="{ options: item.extra.category }"
+        />
+        <property
+          prop="tagIds"
+          name="标签"
+          type="check"
+          :options="{ options: item.extra.tag }"
+        />
+        <property prop="createdAt" name="创建时间" readonly type="date" />
+        <property prop="content" name="文章内容" type="editor" />
+      </property-table>
     </div>
   </el-main>
 </template>
